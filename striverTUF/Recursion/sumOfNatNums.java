69b68237006dc8355cefbe3248423b5d13eeb266
Nickolas Piraino <kwnp17c@gmail.com>
2024-07-17 12:58:28 -0400

    Sum of n numbers formula n*(n+1)/2

---
+++ striverTUF/Recursion/sumOfNatNums.java
@@ -0,0 +1,10 @@
+public class Solution{
+    public static void main(String[] args){
+        solve(5);
+        solve(6);
+    }
+    public static void solve(int n){
+        int sum = n*(n+1)/2
+        System.out.println("The sum of the first " + n + " numbers is: " + sum);
+    }
+}
