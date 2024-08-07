31552b587054bb8da85a4c11c2b66d1f242454f1
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-07 19:55:41 -0400

    added insertion sort and the runtimes

--- striverTUF/Sorting/SelectionSort.java
+++ striverTUF/Sorting/SelectionSort.java
@@ -1,5 +1,5 @@
 class Sort{
-    static void SelectionSort(int []arr, int n){
+    static void SelectionSort(int[] arr, int n){   //O(n^2)
         for (int i = 0; i < n - 1; i++) {
             int mini = i;
             for (int j = i + 1; j < n; j++) {
@@ -14,7 +14,7 @@ class Sort{
         }
     }
 
-    static void BubbleSort(){
+    static void BubbleSort(int[] arr, int n){   //O(n^2) - best(n)
         for (int i = n - 1; i >= 0; i--) {
             int didSwap = 0;
 
@@ -28,6 +28,18 @@ class Sort{
             }
             if (didSwap == 0) {
                 break;
+            }
+        }
+    }
+
+    static void InsertionSort(int[] arr, int n){
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
             }
         }
     }
