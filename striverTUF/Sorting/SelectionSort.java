276a3209c9f916149edfa173939937a6d8838c0a
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-07 19:50:09 -0400

    Sorting algorithms - insertion and bubble sort

---
+++ striverTUF/Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+class Sort{
+    static void SelectionSort(int []arr, int n){
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void BubbleSort(){
+        for (int i = n - 1; i >= 0; i--) {
+            int didSwap = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    didSwap = 1;
+                }
+            }
+            if (didSwap == 0) {
+                break;
+            }
+        }
+    }
+}
