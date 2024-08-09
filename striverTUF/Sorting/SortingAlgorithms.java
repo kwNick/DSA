4d34ec66525310b90a7fb8f40c06613053226d2a
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-09 15:50:29 -0400

    reviewing merge/insertion sort and comments

--- striverTUF/Sorting/SortingAlgorithms.java
+++ striverTUF/Sorting/SortingAlgorithms.java
@@ -35,7 +35,7 @@ class Sort{
     static void InsertionSort(int[] arr, int n){   //O(n^2) - best(n)
         for (int i = 0; i <= n - 1; i++) {
             int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
+            while (j > 0 && arr[j - 1] > arr[j]) {  //backtrack all the elements to find a swap
                 int temp = arr[j - 1];
                 arr[j - 1] = arr[j];
                 arr[j] = temp;
@@ -46,25 +46,25 @@ class Sort{
 
     static void MergeSort(){        //Time:O(N*log_2(N)) space: O(N)
         public static void MSort(int[] arr, int low, int high){
-            if(low >= high)
+            if(low >= high) //base case for a single element which is sorted
                 return;
             int mid = (low+high)/2;
-            MSort(arr, low, mid);
-            MSort(arr, mid+1, high);
+            MSort(arr, low, mid);   //left
+            MSort(arr, mid+1, high); //right
             Merge(arr, low, mid, high);
         }
 
         public static void Merge(int[] arr, int low, int mid, int high){
             ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-            int left = low;      // starting index of left half of arr
+            int left = low;      // starting index of left half of arr      //pointers to the beginning of each array
             int right = mid + 1;   // starting index of right half of arr
     
             //storing elements in the temporary array in a sorted manner//
-            while (left <= mid && right <= high) {
-                if (arr[left] <= arr[right]) {
+            while (left <= mid && right <= high) {  //if either of the pointers runs out of range then break
+                if (arr[left] <= arr[right]) {  //left array pointer is smaller 
                     temp.add(arr[left]);
                     left++;
-                } else {
+                } else {                        //right array pointer is smaller
                     temp.add(arr[right]);
                     right++;
                 }
