1b4b99711cf054d329f0f0267b54e3843258608d
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-14 14:25:30 -0400

    quick sort algorithm and time complexity

--- striverTUF/Sorting/SortingAlgorithms.java
+++ striverTUF/Sorting/SortingAlgorithms.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sort{
     static void SelectionSort(int[] arr, int n){   //O(n^2)
         for (int i = 0; i < n - 1; i++) {
@@ -33,7 +35,7 @@ public class Sort{
     }
 
     static void InsertionSort(int[] arr, int n){   //O(n^2) - best(n)
-        for (int i = 0; i <= n - 1; i++) {
+        for (int i = 0; i <= n - 1; i++) {   //could probly start at i=1 does nothing on the i=0 loop??
             int j = i;
             while (j > 0 && arr[j - 1] > arr[j]) {  //backtrack all the elements to find a swap
                 int temp = arr[j - 1];
@@ -46,7 +48,7 @@ public class Sort{
 
     static void MergeSort(){        //Time:O(N*log_2(N)) space: O(N)
 
-        private static void MSort(int[] arr, int low, int high){
+        private static void MSort(int[] arr, int low, int high){   //"low"-starting index always 0; "high"-length of arr;
             if(low >= high) //base case for a single element which is sorted
                 return;
             int mid = (low+high)/2;
@@ -86,6 +88,40 @@ public class Sort{
             // transfering all elements from temporary to arr //
             for (int i = low; i <= high; i++) {
                 arr[i] = temp.get(i - low);
+            }
+        }
+    }
+    static void QuickSort(){        //Time:O(N*log_2(N)) space: O(1) not taking into account stack space
+        private static int partition(List<Integer> arr, int low, int high) {
+            int pivot = arr.get(low);
+            int i = low;
+            int j = high;
+    
+            while (i < j) {
+                while (arr.get(i) <= pivot && i <= high - 1) {
+                    i++;
+                }
+    
+                while (arr.get(j) > pivot && j >= low + 1) {
+                    j--;
+                }
+                if (i < j) {
+                    int temp = arr.get(i);
+                    arr.set(i, arr.get(j));
+                    arr.set(j, temp);
+                }
+            }
+            int temp = arr.get(low);
+            arr.set(low, arr.get(j));
+            arr.set(j, temp);
+            return j;
+        }
+    
+        private static void qs(List<Integer> arr, int low, int high) {
+            if (low < high) {
+                int pIndex = partition(arr, low, high);
+                qs(arr, low, pIndex - 1);
+                qs(arr, pIndex + 1, high);
             }
         }
     }
