c323de3fcda817db12f43f3b2b67f26840aedb93
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-21 17:06:52 -0400

    inserting a node at the front of the list

--- striverTUF/LinkedLists/SinglyLinkedList.java
+++ striverTUF/LinkedLists/SinglyLinkedList.java
@@ -13,13 +13,33 @@ class Node {
     }
 }
 public class Main {
+    // Function to print the linked list
+    public static void printLL(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+    // Function to insert a new node at the head of the linked list
+    public static Node insertHead(Node head, int val) {
+        Node temp = new Node(val, head);
+        return temp;
+    }
     public static void main(String[] args) {
-        int[] arr = {2, 5, 8, 7};
-        // Create a Node 'x' with the first element of the array
-        Node x = new Node(arr[0]);
-        // Create a reference 'y' pointing to the same Node 'x'
-        Node y = x;
-        // Print the reference 'y', which represents the memory address of the Node 'x'
-        System.out.println(y);
+        // Sample array and value for insertion
+        List<Integer> arr = Arrays.asList(12, 8, 5, 7);
+        int val = 100;
+
+        // Creating a linked list with initial elements from the array
+        Node head = new Node(arr.get(0));
+        head.next = new Node(arr.get(1));
+        head.next.next = new Node(arr.get(2));
+        head.next.next.next = new Node(arr.get(3));
+
+        // Inserting a new node at the head of the linked list
+        head = insertHead(head, val);
+
+        // Printing the linked list
+        printLL(head);
     }
 }
