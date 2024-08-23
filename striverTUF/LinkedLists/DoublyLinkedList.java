f655592c7b9a4ebfa49f54d1b65d2a8627bae2bb
Nickolas Piraino <kwnp17c@gmail.com>
2024-08-23 14:54:53 -0400

    inserting at the end of a doubly linked list given a value and the head of the linked list; O(N) time, must traverse to the end

--- striverTUF/LinkedLists/DoublyLinkedList.java
+++ striverTUF/LinkedLists/DoublyLinkedList.java
@@ -16,4 +16,71 @@ public class Node {
         this.next = null;
         this.back = null;
     }
+
+    private static Node convertArr2DLL(int[] arr) {
+        // Create the head node with the first element of the array
+        Node head = new Node(arr[0]);
+        // Initialize 'prev' to the head node
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            // Create a new node with data from the array and set its 'back' pointer to the previous node
+            Node temp = new Node(arr[i], null, prev);
+            // Update the 'next' pointer of the previous node to point to the new node
+            prev.next = temp;
+            // Move 'prev' to the newly created node for the next iteration
+            prev = temp;
+        }
+        // Return the head of the doubly linked list
+        return head;
+    }
+
+    private static void print(Node head) {
+        while (head != null) {
+            // Print the data in the current node
+            System.out.print(head.data + " ");
+            // Move to the next node
+            head = head.next; // Move to the next node
+        }
+        System.out.println();
+    }
+    
+    // Function to insert a new node with value 'k' at the end of the doubly linked list
+    private static Node insertAtTail(Node head, int k) {  //O(N) time complexity. Must traverse the list to get to the tail
+        // Create a new node with data 'k'
+        Node newNode = new Node(k);
+    
+        // If the doubly linked list is empty, set 'head' to the new node
+        if (head == null) {
+            return newNode;
+        }
+    
+        // Traverse to the end of the doubly linked list
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+    
+        // Connect the new node to the last node in the list
+        current.next = newNode;
+        newNode.back = current;
+    
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 5, 6, 8, 4};
+        // Convert the array to a doubly linked list
+        Node head = convertArr2DLL(arr);
+        
+        // Print the doubly linked list
+         System.out.println("Doubly Linked List Initially: ");
+        print(head);
+
+        System.out.println("Doubly Linked List After Inserting before the node with value 8:");
+
+        head = insertAtTail(head, 10); // Insert a node with value 10 at the end
+        print(head);
+
+    }
 }
